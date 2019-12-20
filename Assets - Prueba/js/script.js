a78ee604c5bc799cf677603e3c3fb941164f41b0
ignacio <berberian95@gmail.com>
2019-12-20 11:55:32 -0300

    first commit portfolio

---
+++ Assets - Prueba/js/script.js
@@ -0,0 +1,19 @@
+$(function () {
+
+    $("a").click(function (event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+
+            var gato = this.hash;
+
+            $("html, body").animate({
+                scrollTop: $(gato).offset().top
+            }, 700, function () {
+                window.location.hash = gato;
+            });
+        }
+
+    });
+    $('[data-toggle="popover"]').popover()
+
+});
